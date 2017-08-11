2683cb8589680df949991dc4df847e9f654fd556
Tony Konzel <saxxonpike@gmail.com>
2017-08-11 16:01:28 -0500

    WIP

---
+++ Source/RhythmCodex.Djmain/Model/ExportedDjmainSample.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RhythmCodex.Djmain.Model
+{
+    public struct ExportedDjmainSample
+    {
+        public DjmainSampleDefinition Definition { get; set; }
+        public byte[] Data { get; set; }
+    }
+}
