a69138b0a8e3b1e4d87e02c061c2e145a88e9efa
SaxxonPike <saxxonpike@gmail.com>
2019-05-14 21:29:07 -0500

    Add an Mjpeg heuristic. (Digital 573 Movies)

---
+++ Source/RhythmCodex.Lib/Mjpeg/Heuristics/MjpegHeuristic.cs
@@ -0,0 +1,51 @@
+using System;
+using RhythmCodex.Heuristics;
+using RhythmCodex.Infrastructure;
+using RhythmCodex.IoC;
+
+namespace RhythmCodex.Mjpeg.Heuristics
+{
+    [Service]
+    public class MjpegHeuristic : IHeuristic
+    {
+        public string Description => "MJPEG Movie";
+        public string FileExtension => "mjpg";
+
+        public HeuristicResult Match(ReadOnlySpan<byte> data)
+        {
+            if (data[0x0000] == 0x00 && data[0x0001] == 0x00)
+                return null;
+            
+            if ((data[0x0000] & 0x1F) != 0)
+                return null;
+
+            if (data[0x0001] >= 0x20)
+                return null;
+
+            if (data[0x0002] != 0x00)
+                return null;
+
+            if (data[0x0003] != 0x38)
+                return null;
+
+            if (data[0x2000] == 0x00 && data[0x2001] == 0x00)
+                return null;
+
+            if ((data[0x2000] & 0x1F) != 0)
+                return null;
+
+            if (data[0x2001] >= 0x20)
+                return null;
+
+            if (data[0x2002] != 0x00)
+                return null;
+
+            if (data[0x2003] != 0x38)
+                return null;
+            
+            return new HeuristicResult(this);
+        }
+
+        public int MinimumLength => 0x4000;
+    }
+}
