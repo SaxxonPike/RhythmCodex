4ed887b9998f083ee269ecb85c64e709d3b64cc5
SaxxonPike <saxxonpike@gmail.com>
2019-05-19 12:10:51 -0500

    Add a Step1 heuristic.

---
+++ Source/RhythmCodex.Lib.Test/Step1/Heuristics/Step1HeuristicTests.cs
@@ -0,0 +1,22 @@
+using System.Linq;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace RhythmCodex.Step1.Heuristics
+{
+    public class Step1HeuristicTests : BaseUnitTestFixture<Step1Heuristic>
+    {
+        [Test]
+        [TestCase("picky")]
+        [TestCase("solo")]
+        [TestCase("2nd")]
+        public void Match_ShouldMatchStep1Files(string chartName)
+        {
+            var data = GetArchiveResource($"Step1.{chartName}.zip")
+                .First()
+                .Value;
+
+            Subject.Match(data).Should().NotBeNull();
+        }
+    }
+}
