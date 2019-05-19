d416a6c4a94e1a4dd50c2dad64b4d63a3f37cb7a
SaxxonPike <saxxonpike@gmail.com>
2019-05-19 01:01:31 -0500

    Preliminary PSF work.

---
+++ Source/RhythmCodex.Lib/Psf/Streamers/IPsfStreamReader.cs
@@ -0,0 +1,10 @@
+using System.IO;
+using RhythmCodex.Psf.Models;
+
+namespace RhythmCodex.Psf.Streamers
+{
+    public interface IPsfStreamReader
+    {
+        PsfChunk Read(Stream source);
+    }
+}
