ec053caa32cf20373275e4916f0a8762c8493bf5
SaxxonPike <saxxonpike@gmail.com>
2019-05-30 16:56:46 -0500

    Break out ADPCM constants.

--- Source/RhythmCodex.Lib/ImaAdpcm/Converters/ImaAdpcmDecoder.cs
+++ Source/RhythmCodex.Lib/ImaAdpcm/Converters/ImaAdpcmDecoder.cs
@@ -16,27 +16,6 @@ namespace RhythmCodex.ImaAdpcm.Converters
     {
         // Reference: https://github.com/dbry/adpcm-xq/blob/master/adpcm-lib.c
 
-        private static readonly int[] StepTable =
-        {
-            7, 8, 9, 10, 11, 12, 13, 14,
-            16, 17, 19, 21, 23, 25, 28, 31,
-            34, 37, 41, 45, 50, 55, 60, 66,
-            73, 80, 88, 97, 107, 118, 130, 143,
-            157, 173, 190, 209, 230, 253, 279, 307,
-            337, 371, 408, 449, 494, 544, 598, 658,
-            724, 796, 876, 963, 1060, 1166, 1282, 1411,
-            1552, 1707, 1878, 2066, 2272, 2499, 2749, 3024,
-            3327, 3660, 4026, 4428, 4871, 5358, 5894, 6484,
-            7132, 7845, 8630, 9493, 10442, 11487, 12635, 13899,
-            15289, 16818, 18500, 20350, 22385, 24623, 27086, 29794,
-            32767
-        };
-
-        private static readonly int[] IndexTable =
-        {
-            -1, -1, -1, -1, 2, 4, 6, 8
-        };
-
         public IList<ISound> Decode(ImaAdpcmChunk chunk)
         {
             var sounds = new List<ISound>();
@@ -79,7 +58,7 @@ namespace RhythmCodex.ImaAdpcm.Converters
             {
                 float DecodeNybble(int data)
                 {
-                    var step = StepTable[control];
+                    var step = ImaAdpcmConstants.StepTable[control];
                     var delta = step >> 3;
 
                     if ((data & 0x01) != 0) delta += (step >> 2);
@@ -93,7 +72,7 @@ namespace RhythmCodex.ImaAdpcm.Converters
                         sample = 32767;
                 
                     sample += delta;
-                    control += IndexTable[data & 0x7];
+                    control += ImaAdpcmConstants.IndexTable[data & 0x7];
                     if (control < 0)
                         control = 0;
                     if (control > 88)
