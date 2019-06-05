09e9dbb41f3fd32be25c98c9a65bbfb374bd2abf
SaxxonPike <saxxonpike@gmail.com>
2019-06-05 09:21:42 -0500

    Test MDB decoding.

---
+++ Source/RhythmCodex.Lib.Test/Ddr/Integration/Ddr573SongDatabaseIntegrationTests.cs
@@ -0,0 +1,34 @@
+using System;
+using System.IO;
+using System.Linq;
+using FluentAssertions;
+using NUnit.Framework;
+using RhythmCodex.Compression;
+using RhythmCodex.Ddr.Converters;
+using RhythmCodex.Infrastructure;
+
+namespace RhythmCodex.Ddr.Integration
+{
+    public class Ddr573SongDatabaseIntegrationTests : BaseIntegrationFixture
+    {
+        [Test]
+        public void Test1()
+        {
+            var inputArchive = GetArchiveResource($"Ddr.mdb.zip");
+            var data = inputArchive
+                .First(name => name.Key.Equals("mdb", StringComparison.OrdinalIgnoreCase))
+                .Value;
+            var expected = inputArchive
+                .First(name => name.Key.Equals("expected", StringComparison.OrdinalIgnoreCase))
+                .Value;
+
+            var decrypter = Resolve<IDdr573DatabaseDecrypter>();
+            var decompressor = Resolve<IBemaniLzDecoder>();
+
+            var decrypted = decrypter.Decrypt(data, "EXTREME");
+            var decompressed = decompressor.Decode(new MemoryStream(decrypted));
+
+            decompressed.Should().BeEquivalentTo(expected);
+        }
+    }
+}
