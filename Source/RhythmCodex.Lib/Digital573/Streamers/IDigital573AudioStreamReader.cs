c065de4f3d19bf1ea89ffe2c3e827b25056f650f
SaxxonPike <saxxonpike@gmail.com>
2019-06-08 13:34:38 -0500

    Remove dead code.

--- Source/RhythmCodex.Lib/Digital573/Streamers/IDigital573AudioStreamReader.cs
+++
@@ -1,9 +0,0 @@
-using System.IO;
-
-namespace RhythmCodex.Digital573.Streamers
-{
-    public interface IDigital573AudioStreamReader
-    {
-        byte[] Read(Stream stream, long length);
-    }
-}
