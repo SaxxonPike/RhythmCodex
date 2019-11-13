0b4459e0c4fd56edab7906171ea7b12c993f09a6
SaxxonPike <saxxonpike@gmail.com>
2019-11-13 17:11:57 -0600

    Let's multi-target for .NET Standard 2.0 and 2.1 so as to be linkable from .NET Framework 4.8

--- Source/RhythmCodex.Cli/RhythmCodex.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-dotnet RhythmCodex.Cli.dll "$@"
--- publish.sh
+++ publish.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-dotnet publish -c Release -f netcoreapp2.0 -o Release Source/RhythmCodex.Cli/RhythmCodex.Cli.csproj
+dotnet publish -c Release -f netcoreapp3.0 -o Release Source/RhythmCodex.Cli/RhythmCodex.Cli.csproj
