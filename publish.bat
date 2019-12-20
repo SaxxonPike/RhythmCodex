eb883f826229ada04fef8e5b9106a7dc30be087a
SaxxonPike <saxxonpike@gmail.com>
2019-12-20 16:06:55 -0600

    Build for .NET 4.8 and .NET Core 3

--- publish.bat
+++ publish.bat
@@ -1,1 +1,2 @@
-dotnet publish -c Release -f netcoreapp3.0 -o Release Source/RhythmCodex.Cli/RhythmCodex.Cli.csproj
+dotnet publish -c Release -f net48 -o Release/net48 Source/RhythmCodex.Cli/RhythmCodex.Cli.csproj
+dotnet publish -c Release -f netcoreapp3.0 -o Release/netcoreapp3.0 Source/RhythmCodex.Cli/RhythmCodex.Cli.csproj
