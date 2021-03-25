8da4c8bed90bd15d6d5675906b73816c4c44a69b
Anthony Konzel <saxxonpike@gmail.com>
2021-03-25 16:28:41 -0500

    Missed a couple files.

---
+++ Source/XwbExtract/Program.cs
@@ -0,0 +1,12 @@
+﻿using ClientCommon;
+
+namespace XwbExtract
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Bootstrap.StartApp<App>(args);
+        }
+    }
+}
