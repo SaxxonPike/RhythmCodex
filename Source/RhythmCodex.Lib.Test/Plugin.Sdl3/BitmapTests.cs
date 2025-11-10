0974f22751ea1e02c071c27f6f1fa08a67c7a00c
Saxxon Fox <saxxonpike@gmail.com>
2025-11-09 18:52:10 -0600

    Test name refactor

--- Source/RhythmCodex.Lib.Test/Plugin.Sdl3/BitmapTests.cs
+++ Source/RhythmCodex.Lib.Test/Plugin.Sdl3/BitmapTests.cs
@@ -6,10 +6,10 @@ using Shouldly;
 namespace RhythmCodex.Plugin.Sdl3;
 
 [TestFixture]
-public class BitmapTests : BaseIntegrationFixture<BitmapStreamReader>
+public class BitmapStreamReaderTests : BaseIntegrationFixture<BitmapStreamReader>
 {
     [Test]
-    public void test1()
+    public void Read_ReadsPng()
     {
         var stream = new MemoryStream(GetEmbeddedResource("Png.example.png"));
         var bitmap = Subject.Read(stream);
