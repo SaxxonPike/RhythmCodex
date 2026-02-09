eaf11be9f730ef6d630eb20ddffd3deb99e1b3f5
Saxxon Fox <saxxonpike@gmail.com>
2026-02-09 03:20:34 -0600

    Fix heuristic breakage for beatmania Club Mix

--- Source/RhythmCodex.Lib/Archs.Djmain/Heuristics/DjmainHddDescriptionHeuristic.cs
+++ Source/RhythmCodex.Lib/Archs.Djmain/Heuristics/DjmainHddDescriptionHeuristic.cs
@@ -39,24 +39,24 @@ public class DjmainHddDescriptionHeuristic : IDjmainHddDescriptionHeuristic
 
         DjmainChunkFormat GetFormat(string formatId)
         {
-            switch (formatId)
+            switch (formatId.Trim())
             {
-                case " GQ753": return DjmainChunkFormat.BeatmaniaFirst;
-                case " GX853": return DjmainChunkFormat.BeatmaniaSecond;
-                case " GQ825": return DjmainChunkFormat.BeatmaniaThird;
-                case " GQ847": return DjmainChunkFormat.BeatmaniaFourth;
-                case " GQ981": return DjmainChunkFormat.BeatmaniaFifth;
-                case " GCA21": return DjmainChunkFormat.BeatmaniaSixth;
-                case " GEB07": return DjmainChunkFormat.BeatmaniaSeventh;
-                case " GQ993": return DjmainChunkFormat.BeatmaniaClub;
-                case " GQ858": return DjmainChunkFormat.BeatmaniaComplete;
-                case "GQ988 ": return DjmainChunkFormat.BeatmaniaComplete2;
-                case " GQA05": return DjmainChunkFormat.BeatmaniaCore;
-                case " GQ995": return DjmainChunkFormat.BeatmaniaDct;
-                case " GCC01": return DjmainChunkFormat.BeatmaniaFinal;
-                case " GQ803": return DjmainChunkFormat.Popn1;
-                case " GQ831": return DjmainChunkFormat.Popn2;
-                case " GQ980": return DjmainChunkFormat.Popn3;
+                case "GQ753": return DjmainChunkFormat.BeatmaniaFirst;
+                case "GX853": return DjmainChunkFormat.BeatmaniaSecond;
+                case "GQ825": return DjmainChunkFormat.BeatmaniaThird;
+                case "GQ847": return DjmainChunkFormat.BeatmaniaFourth;
+                case "GQ981": return DjmainChunkFormat.BeatmaniaFifth;
+                case "GCA21": return DjmainChunkFormat.BeatmaniaSixth;
+                case "GEB07": return DjmainChunkFormat.BeatmaniaSeventh;
+                case "GQ993": return DjmainChunkFormat.BeatmaniaClub;
+                case "GQ858": return DjmainChunkFormat.BeatmaniaComplete;
+                case "GQ988": return DjmainChunkFormat.BeatmaniaComplete2;
+                case "GQA05": return DjmainChunkFormat.BeatmaniaCore;
+                case "GQ995": return DjmainChunkFormat.BeatmaniaDct;
+                case "GCC01": return DjmainChunkFormat.BeatmaniaFinal;
+                case "GQ803": return DjmainChunkFormat.Popn1;
+                case "GQ831": return DjmainChunkFormat.Popn2;
+                case "GQ980": return DjmainChunkFormat.Popn3;
                 default: return DjmainChunkFormat.Unknown;
             }
         }
