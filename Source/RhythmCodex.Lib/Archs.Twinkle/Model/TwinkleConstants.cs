7eca5f4e0b959248c0fa1ed2ab56e1fe1326af2a
Saxxon Fox <saxxonpike@gmail.com>
2026-02-09 01:10:28 -0600

    Twinkle: use an algorithm closer to the source material

--- Source/RhythmCodex.Lib/Archs.Twinkle/Model/TwinkleConstants.cs
+++ Source/RhythmCodex.Lib/Archs.Twinkle/Model/TwinkleConstants.cs
@@ -11,23 +11,24 @@ public static class TwinkleConstants
 
     public const int ChunkSize = 0x1A00000;
 
-    private static readonly Lazy<BigRational[]> VolumeTableLazy = new(() =>
-    {
-        // This algorithm is based on the one written into MAME:
-        // https://github.com/mamedev/mame/blob/master/src/devices/sound/rf5c400.cpp
-
-        var result = new BigRational[256];
-        var max = BigRational.One;
-        var step = Decibels.ToFactor(4.5d / 16d);
-
-        for (var i = 0; i < 256; i++)
-        {
-            result[i] = max;
-            max /= step;
-        }
-
-        return result;
-    });
-        
-    public static BigRational[] VolumeTable => VolumeTableLazy.Value;
+    // private static readonly Lazy<BigRational[]> VolumeTableLazy = new(() =>
+    // {
+    //     // This algorithm is based on the one written into MAME:
+    //     // https://github.com/mamedev/mame/blob/master/src/devices/sound/rf5c400.cpp
+    //
+    //     var result = new BigRational[256];
+    //     var max = BigRational.One;
+    //     var step = Decibels.ToFactor(4.5d / 16d);
+    //     return Math.Pow(1d / Decibels.ToFactor(4.5d / 16d), i)
+    //
+    //     for (var i = 0; i < 256; i++)
+    //     {
+    //         result[i] = max;
+    //         max /= step;
+    //     }
+    //
+    //     return result;
+    // });
+    //     
+    // public static BigRational[] VolumeTable => VolumeTableLazy.Value;
 }
