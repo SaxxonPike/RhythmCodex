4db8fccc34ce96f3fbfb272b3d9ae0ef4d1e4895
Saxxon Fox <saxxonpike@gmail.com>
2026-02-09 01:08:34 -0600

    DSP: Add Twinkle volume calculation to the beatmania DSP service

--- Source/RhythmCodex.Lib/Games.Beatmania/Converters/IBeatmaniaDspTranslator.cs
+++ Source/RhythmCodex.Lib/Games.Beatmania/Converters/IBeatmaniaDspTranslator.cs
@@ -6,6 +6,7 @@ public interface IBeatmaniaDspTranslator
 {
     BigRational GetLinearVolume(int volume);
     BigRational GetDjmainVolume(int volume);
+    BigRational GetTwinkleVolume(int volume);
     BigRational GetBm2dxPanning(int panning);
     BigRational GetDjmainPanning(int panning);
     BigRational GetDjmainRate(int rate);
